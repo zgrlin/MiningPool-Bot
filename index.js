af9fa2c936af99195f3593133d3680d29d64d4df
zgur.ETH <o@zgur.org>
2022-01-14 18:34:20 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord=require("discord.js");
+const client=new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const config = require("./config.json")
+
+client.on("message", message=>{
+    console.log('Bot is ready!')
+
+    console.log("Read message: "+message.content)
+
+    if(message.content==="!hello")
+    message.channel.send("hello")
+
+    if(message.content==="!ping")
+    message.channel.send("pong")
+
+    if(message.content==="!avatar")
+    message.reply(message.author.displayAvatarURL())
+})
+
+client.login(config.token)
